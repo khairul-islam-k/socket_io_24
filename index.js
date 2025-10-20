2187ff9674e24293a62923533dcd224fe4e47555
Khairul <khairul.web1@gmail.com>
2025-10-20 14:19:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path")
+const expressServer = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
+const port = process.env.PORT || 3000;
+
+io.on("connection",(socket) => {
+  socket.on("chat",(data) => {
+    
+    io.emit("chat_transfer",data);
+
+  })
+})
+
+
+app.get("/",(req, res) => {
+    // res.sendFile(__dirname + "/index.html");
+    res.send("socket.io server");
+})
+
+expressServer.listen(port, () => {
+  console.log(`Example app listening on port${port}`)
+})
